c2765b9346b5a987e41585b01bccc57d100557e6
Lavion <ilavion4u@gmail.com>
2024-12-30 18:05:44 +0100

    Add project files.

---
+++ Miya/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Miya
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Miya/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Miya
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
